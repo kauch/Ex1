eb9b6f4dd224744384d139750014c34aa4661f4e
Kert <Kert@192.168.1.51>
2018-12-17 00:26:48 +0300

    add injector

---
+++ nc-ex_1/ex1/src/main/java/com/netcracker/injector/Injector.java
@@ -0,0 +1,50 @@
+package com.netcracker.injector;
+
+import com.netcracker.annotation.AutoInjectable;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.util.Properties;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Injector {
+	private static Logger log = LogManager.getLogger(Injector.class);
+    private Properties properties;
+
+    public Injector(){
+        properties = new Properties();
+        InputStream fis = getClass().getClassLoader().getResourceAsStream("app.properties");
+
+        try {
+            if(fis!=null){
+                properties.load(fis);
+            }
+        } catch (FileNotFoundException e) {
+        	log.info("FileNotFoundException", e);
+        } catch (IOException e) {
+        	log.info("IOException", e);
+        }
+    }
+
+    public Object inject(Object object){
+        for(Field field : object.getClass().getDeclaredFields()){
+            field.setAccessible(true);								 // Отмена проверок доступа
+            if(field.isAnnotationPresent(AutoInjectable.class)){
+                try {
+                    field.set(object, Class.forName(properties.getProperty(field.getType().getName())).newInstance());
+                } catch (IllegalAccessException e) {
+                	log.info("IllegalAccessException", e);
+                } catch (InstantiationException e) {
+                	log.info("InstantiationException", e);
+                } catch (ClassNotFoundException e) {
+                	log.info("ClassNotFoundException", e);
+                }
+            }
+        }
+
+        return object;
+    }
+}
